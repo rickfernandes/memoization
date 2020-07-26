949afe4c1adf1611551b6853f4ce5494c6ed8a05
Ricardo Fernandes <rs.bonafe@gmail.com>
2020-07-26 13:10:19 +0100

    Final version of Fibonacci with memoization

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,10 @@
 from functools import lru_cache
-import time
+from time import perf_counter
 
 # Fibonacci sequence starting with 1,1
 # In this case for every recursion all numbers are re-calculated
 def fib(n):
-	if n <=1:
-		return 1
-	return fib(n-1) + fib(n-2)
+	return 1 if n <= 1 else fib(n-1) + fib(n-2)
 
 # Fibonacci sequence with memoization using dict
 # In this case for every recursion we check if the result is present
@@ -24,23 +22,22 @@ def fib_dict(n):
 # it has the same behaviour as above but we don't need to use a global dict
 @lru_cache(maxsize = 2000)
 def fib_lru(n):
-	if n <=1:
-		return 1
-	return fib_lru(n-1) + fib_lru(n-2)
+	return 1 if n <= 1 else fib_lru(n-1) + fib_lru(n-2)
 
 # Timing the functions for a number
 def fibonacci_times(n):
+	# Array with all fibonacci functions to iterate through
 	fibs = [fib,fib_dict,fib_lru]
-	print()
+	# Using perf_counter to get the time in seconds
 	for fibonacci in fibs:
-		t_start = time.perf_counter()
+		t_start = perf_counter()
 		temp_f = fibonacci(n)
-		t_end = time.perf_counter()
-		print(f"Function {fibonacci.__name__} for {n} with result {temp_f} took {t_end - t_start:0.4f} in seconds")
-	print()
+		t_end = perf_counter()
+		print(f"Function {fibonacci.__name__} for {n+1} with result {temp_f} took {t_end - t_start:0.4f} in seconds")
 
 
 def main():
+	# Defining a input loop that forces the user to input an integer
 	n = ''
 	while not isinstance(n,int):
 		n = input('Insert a number to check Fibonacci times: ')
@@ -48,7 +45,9 @@ def main():
 			n = int(n)
 		except:
 			print('Invalid input')
-	fibonacci_times(n)
+	print(f"\nFibonacci array: {[fib_lru(n) for n in range(n)]}\n")
+	# print('\n','Fibonacci array: ',[fib_lru(n) for n in range(n)])
+	fibonacci_times(n-1)
 
 if __name__ == '__main__':
 	main()
